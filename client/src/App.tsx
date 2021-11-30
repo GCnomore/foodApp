9a52c711a0e2184e6806d170b916c8cd9ca2439c
isaac <isaacgc08@gmail.com>
2021-11-30 13:49:05 -0800

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,32 +15,32 @@ import ResultPage from "./pages/Result/Result_Page";
 
 export default function App() {
   return (
-      <AppContainer className="App">
-        <Router>
-          <Switch>
-            <Route path={ROUTES.HOME_PAGE} exact component={HomePage} />
-            <Route
-              path={ROUTES.RESULT_PAGE}
-              exact
-              component={ResultPage}
-            />
-            <Route path={ROUTES.RECIPE_PAGE} exact component={RecipePage} />
-            <Route path={ROUTES.PROFILE_PAGE} exact component={ProfilePage} />
-            <Route path={ROUTES.LOGIN_PAGE} exact component={LoginPage} />
-            <Route path={ROUTES.REGISTER_PAGE} exact component={RegisterPage} />
-            <Route path={ROUTES.SAVE_EARTH} exact component={SaveEarth} />
-          </Switch>
-          <NavBar>
-            <div>
-              <Link to={ROUTES.HOME_PAGE}>Home</Link>
-              <Link to={ROUTES.REGISTER_PAGE}>Register</Link>
-              <Link to={ROUTES.LOGIN_PAGE}>Login</Link>
-              <Link to={ROUTES.SAVE_EARTH}>
-                Save <FontAwesomeIcon icon={faGlobeAmericas} />
-              </Link>
-            </div>
-          </NavBar>
-        </Router>
-      </AppContainer>
+    <AppContainer className="App">
+      <Router>
+        <Switch>
+          <Route path={ROUTES.HOME_PAGE} exact component={HomePage} />
+          <Route path={ROUTES.RESULT_PAGE} exact component={ResultPage} />
+          <Route
+            path={`${ROUTES.RECIPE_PAGE}/:id`}
+            exact
+            component={RecipePage}
+          />
+          <Route path={ROUTES.PROFILE_PAGE} exact component={ProfilePage} />
+          <Route path={ROUTES.LOGIN_PAGE} exact component={LoginPage} />
+          <Route path={ROUTES.REGISTER_PAGE} exact component={RegisterPage} />
+          <Route path={ROUTES.SAVE_EARTH} exact component={SaveEarth} />
+        </Switch>
+        <NavBar>
+          <div>
+            <Link to={ROUTES.HOME_PAGE}>Home</Link>
+            <Link to={ROUTES.REGISTER_PAGE}>Register</Link>
+            <Link to={ROUTES.LOGIN_PAGE}>Login</Link>
+            <Link to={ROUTES.SAVE_EARTH}>
+              Save <FontAwesomeIcon icon={faGlobeAmericas} />
+            </Link>
+          </div>
+        </NavBar>
+      </Router>
+    </AppContainer>
   );
 }

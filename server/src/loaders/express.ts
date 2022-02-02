1379578549f6ded07b1bfcb6556246ddd107613f
isaac <isaacgc08@gmail.com>
2022-02-01 23:33:49 -0800

    api limiter

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,8 +1,24 @@
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+
 import AppRouters from "../api/index";
+import Logger from "./logger";
 
 export default async ({ app }: { app: express.Application }) => {
+  const limiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 15 minutes
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: function (req, res) {
+      Logger.info(`Search limit ::: ${req}`);
+      return res.status(429).json({
+        error: "Search limit triggered.",
+      });
+    },
+  });
+
   app.get("/status", (req, res) => {
     res.status(200).end();
   });
@@ -15,6 +31,8 @@ export default async ({ app }: { app: express.Application }) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  app.use("/recipes", limiter);
+  app.use("/information", limiter);
   app.use(AppRouters());
   return app;
 };
